f97af5b503e557b3218241b9388acb32dc69bdfb
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-12 03:01:20 +0500

    Update app.js

--- first_node_server/app.js
+++ first_node_server/app.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const http = require('http');
+
+let server = http.createServer((req,res)=>{
+    console.log(req);
+})
+
+
+const PORT = 3000
+server.listen(PORT, ()=>{
+    console.log(`server running at http://localhost:${PORT}`);
+})

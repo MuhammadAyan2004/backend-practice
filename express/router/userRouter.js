ad718f257ba3c2e206d8354a6dd45cd56a39b6fe
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-18 00:42:18 +0500

    Create userRouter.js

---
+++ express/router/userRouter.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const path = require('path')
+const userRouter = express.Router()
+const rootDir = require('../utils/path')
+
+userRouter.get('/',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','user.html'))
+})
+
+module.exports = userRouter;

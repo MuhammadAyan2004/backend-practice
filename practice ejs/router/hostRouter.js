56329022feadff70c46e63f4831e0094179fe8e3
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-23 02:39:26 +0500

    Update hostRouter.js

--- practice ejs/router/hostRouter.js
+++ practice ejs/router/hostRouter.js
@@ -1,15 +1,10 @@
 const express = require('express')
 const hostRouter = express.Router()
 
-hostRouter.get('/Addhome.ejs',(req,res)=>{
-    res.render('Addhome',{pageTitle:'Add Home',activePage:'AddHome'})
-})
+const homeController = require('../controllers/homes')
 
-const booking = [] 
-hostRouter.post('/homeRegister.ejs',(req,res)=>{
-    booking.push(req.body)
-    res.render('homeRegister', {pageTitle:'Registration Successful',activePage:'AddHome'})
-})
+
+hostRouter.get('/Addhome.ejs',homeController.getAddHome); 
+hostRouter.post('/homeRegister.ejs',homeController.postHome);
 
 exports.hostRouter = hostRouter;
-exports.booking = booking;

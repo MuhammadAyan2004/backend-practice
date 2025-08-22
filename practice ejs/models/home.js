8f51235617850113710cf295d3d751d2862ee974
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-23 02:39:20 +0500

    Create home.js

---
+++ practice ejs/models/home.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fs = require('fs')
+const dirname = require('../utils/pathUtil')
+
+module.exports = class home {
+    constructor(houseName,price,location,rating,photoUrl){
+        this.houseName = houseName;
+        this.price = price;
+        this.location = location;
+        this.rating = rating;
+        this.photoUrl = photoUrl;
+    }
+    save () {
+        home.fetchAll((booking)=>{
+            booking.push(this)
+            const filePath = path.join(dirname, 'data', 'home.json') 
+            fs.writeFile(filePath, JSON.stringify(booking),err=>{
+                console.log('your err comes here', err);
+            })
+        })
+    }
+    static fetchAll(cb){
+        const filePath = path.join(dirname, 'data', 'home.json') 
+        fs.readFile(filePath,(err,data)=> {
+            cb(!err ? JSON.parse(data) : [])
+        })
+    }   
+}

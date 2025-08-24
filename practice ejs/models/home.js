3c0b5a27353791cb787b3a7f35a4426de611d14a
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-25 02:19:00 +0500

    Update home.js

--- practice ejs/models/home.js
+++ practice ejs/models/home.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const fs = require('fs')
 const dirname = require('../utils/pathUtil')
-
+const filePath = path.join(dirname, 'data', 'home.json') 
 module.exports = class home {
     constructor(houseName,price,location,rating,photoUrl){
         this.houseName = houseName;
@@ -14,14 +14,12 @@ module.exports = class home {
         this.id = Math.random().toString()
         home.fetchAll((booking)=>{
             booking.push(this)
-            const filePath = path.join(dirname, 'data', 'home.json') 
             fs.writeFile(filePath, JSON.stringify(booking),err=>{
                 console.log('your err comes here', err);
             })
         })
     }
     static fetchAll(cb){
-        const filePath = path.join(dirname, 'data', 'home.json') 
         fs.readFile(filePath,(err,data)=> {
             cb(!err ? JSON.parse(data) : [])
         })

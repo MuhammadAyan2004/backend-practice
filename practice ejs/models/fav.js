87e6271c3d4a8b20d4a120df3401f4d4bd4856cd
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-25 02:19:42 +0500

    Create fav.js

---
+++ practice ejs/models/fav.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const fs = require('fs')
+const dirname = require('../utils/pathUtil')
+const { json } = require('stream/consumers')
+const favFilePath = path.join(dirname, 'data', 'fav.json') 
+
+module.exports = class Favorite {
+    static addToFavorite (homeId, cb){
+        Favorite.getFavorites ((fav) => {
+            if(fav.includes(homeId)){
+                cb("home is already exixt");
+            }else{
+                fav.push(homeId)
+                fs.writeFile(favFilePath,JSON.stringify(fav), cb)
+            }
+        })
+    }
+    static getFavorites (cb){
+        fs.readFile(favFilePath, (err,data)=>{
+            cb (!err ? JSON.parse(data) : []) 
+        });
+    }
+}

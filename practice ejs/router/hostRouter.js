e11fe3c244fd4c5cd2ec837eba072c85b003ab11
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-26 23:49:35 +0500

    Update hostRouter.js

--- practice ejs/router/hostRouter.js
+++ practice ejs/router/hostRouter.js
@@ -4,10 +4,12 @@ const hostRouter = express.Router()
 const homeController = require('../controllers/hostController')
 
 
-hostRouter.get('/Addhome',homeController.getAddHome); 
+hostRouter.get('/host/Addhome',homeController.getAddHome); 
+hostRouter.get('/host/homeAdded', homeController.getHomeAdded);
+hostRouter.post('/host/homeRegister',homeController.postHome);
+hostRouter.get('/host/edit-home/:homeId',homeController.getEditHome);
+hostRouter.post('/host/edit-home', homeController.postEditHome);
+hostRouter.post('/host/delete/:homeId', homeController.postDeleteHome);
 
-hostRouter.get('/homeAdded', homeController.getHomeAdded);
-
-hostRouter.post('/homeRegister',homeController.postHome);
 
 exports.hostRouter = hostRouter;

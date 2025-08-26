8b5d190a70297e1d0b0434e5a446dda5dee27fad
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-26 23:49:39 +0500

    Update userRouter.js

--- practice ejs/router/userRouter.js
+++ practice ejs/router/userRouter.js
@@ -4,17 +4,12 @@ const userRouter = express.Router()
 const homeController = require('../controllers/storeController')
 
 userRouter.get('/',homeController.getIndex)
-
 userRouter.get('/home',homeController.gethomeAdd)
-
 userRouter.get('/homeList', homeController.getHomeList)
-
 userRouter.get('/favorite', homeController.getFavoriteList)
-
 userRouter.post('/favorite', homeController.postFavoriteList)
-
+userRouter.post('/favorite/delete/:homeId',homeController.postremoveFav)
 userRouter.get('/bookings', homeController.getBooking)
-
 userRouter.get('/homeList/:homeId',homeController.getHomeDetail)
 
 

51780bed0571f3c73695c7e3f917054e0acfc9e8
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-28 01:27:09 +0500

    modules

---
+++ connecting database sql/router/userRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const userRouter = express.Router()
+
+const homeController = require('../controllers/storeController')
+
+userRouter.get('/',homeController.getIndex)
+userRouter.get('/home',homeController.gethomeAdd)
+userRouter.get('/homeList', homeController.getHomeList)
+userRouter.get('/favorite', homeController.getFavoriteList)
+userRouter.post('/favorite', homeController.postFavoriteList)
+userRouter.post('/favorite/delete/:homeId',homeController.postremoveFav)
+userRouter.get('/bookings', homeController.getBooking)
+userRouter.get('/homeList/:homeId',homeController.getHomeDetail)
+
+
+exports.userRouter = userRouter;

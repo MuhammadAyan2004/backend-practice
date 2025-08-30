5f895d52c9a5c8c734330f54f4a03ec5ad4cf70f
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-31 03:22:53 +0500

    Create hostController.js

---
+++ connect mongoDB/controllers/hostController.js
@@ -0,0 +1,69 @@
+const home = require('../models/home')
+
+exports.getAddHome = (req, res) => {
+    res.render('host/Addhome', {
+        pageTitle: 'Add Home', 
+        activePage: 'AddHome',
+        editing: false
+    })
+}
+
+exports.getHomeAdded = (req, res) => {
+    home.fetchAll().then(registerHome => {
+        console.log(registerHome);
+        res.render('host/homeAdded', {
+            booking: registerHome,
+            pageTitle: 'book homes',
+            activePage: 'homeAdded'
+        })
+    }).catch(err=>{
+        res.redirect('/')
+    })
+}
+exports.postHome = (req, res) => {
+    const { userName, location, price, rating, pic, description } = req.body
+    const registerHome = new home(userName, price, location, rating, pic, description)
+    registerHome.save()
+    res.render('host/homeRegister', {
+        pageTitle: 'Registration Successful',
+        activePage: 'AddHome'
+    })
+}
+exports.getEditHome = (req, res) => {
+    const homeId = req.params.homeId;
+    const editing = req.query.editing === 'true';
+
+    home.findById(homeId).then(hom=>{
+        if(!hom){
+            res.redirect('/host/homeAdded')
+        }else{
+            res.render('host/Addhome', {
+                pageTitle: 'Edit Home', 
+                activePage: 'AddHome',
+                editing: editing,
+                hom:hom
+            })
+        }
+    })
+}
+exports.postEditHome = (req, res) => {
+    const {id, userName, location, price, rating, pic, description } = req.body
+    const registerHome = new home(userName, price, location, rating, pic, description, id)
+
+    registerHome.save()
+    res.redirect('/host/homeAdded')
+
+}
+
+exports.postDeleteHome = (req, res) => {
+    const homeId = req.params.homeId;
+    home.deletebyId(homeId)
+        .then(() => {
+            res.redirect('/host/homeAdded')
+        })
+        .catch((err) => {
+            if (err) {
+                console.log("object has not been deleted");
+            }
+        })
+}

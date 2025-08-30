ee551e1a0d12a5582bb384040df6f5ff3563fcec
Muhammad AYAN <m101ayan92@gmail.com>
2025-08-31 03:22:46 +0500

    Create app.js

---
+++ connect mongoDB/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const { userRouter } = require('./router/userRouter')
+const { hostRouter } = require('./router/hostRouter')
+const errorController = require('./controllers/errorController')
+const {mongoConnect} = require('./utils/databaseUtil')
+const app = express()
+
+app.set('view engine','ejs')
+app.set('views','views')
+
+app.use(express.urlencoded())
+app.use(userRouter)
+app.use(hostRouter)
+
+
+app.use(errorController.PageNotFound)
+app.use('/homeList/',errorController.PageNotFound)
+
+const port = 3001
+mongoConnect(()=>{
+    app.listen(port,()=>{
+        console.log(`server is running on http://localhost:${port}`);
+    })
+})

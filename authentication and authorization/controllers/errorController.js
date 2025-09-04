04bdfa62cd2bac1faedf83b94adc5e0be75d9083
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-04 18:06:51 +0500

    Update errorController.js

--- authentication and authorization/controllers/errorController.js
+++ authentication and authorization/controllers/errorController.js
@@ -1,3 +1,3 @@
 exports.PageNotFound = (req,res)=>{
-    res.status(404).render('404',{pageTitle:'404 page',isLoggedIn: true})
+    res.status(404).render('404',{pageTitle:'404 page',isLoggedIn: true,user: req.session.user})
 }

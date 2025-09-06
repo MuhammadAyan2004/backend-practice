74bd240d12818410fa91868447667db0a6f82267
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-07 01:45:39 +0500

    Create home.js

---
+++ file upload and download/models/home.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const homeScheme = new mongoose.Schema({
+    houseName: {type:String,required:true},
+    price: {type:Number,required:true},
+    location: {type:String,required:true},
+    rating: {type:Number,required:true},
+    photoUrl: String,
+    description: String,
+    hostHomes:{type:mongoose.Schema.Types.ObjectId}
+})
+
+
+module.exports = mongoose.model('home',homeScheme)

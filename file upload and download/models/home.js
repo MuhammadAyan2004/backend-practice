80f3b597224229abeb4ad9f26ce4838345ffa72d
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-13 03:57:46 +0500

    Update home.js

--- file upload and download/models/home.js
+++ file upload and download/models/home.js
@@ -5,8 +5,9 @@ const homeScheme = new mongoose.Schema({
     price: {type:Number,required:true},
     location: {type:String,required:true},
     rating: {type:Number,required:true},
-    img: String,
     description: String,
+    img: {data:Buffer,contentType:String},
+    rulepdf: {data:Buffer,contentType:String},
     hostHomes:{type:mongoose.Schema.Types.ObjectId}
 })
 

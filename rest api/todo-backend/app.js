276898786ca16001792a32a7bc2c2b37379ecaf6
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-17 04:23:02 +0500

    Update app.js

--- rest api/todo-backend/app.js
+++ rest api/todo-backend/app.js
@@ -1,15 +1,19 @@
 const express = require('express')
-const db_Path = "mongodb://127.0.0.1:27017/todo"
+const cors = require('cors')
 const { default: mongoose } = require('mongoose')
 
-
+const db_Path = "mongodb://127.0.0.1:27017/todo"
 const errorController = require('./controllers/errorController')
+const todoRouter = require('./router/todoRoute')
 
 const app = express()
 
 
 app.use(express.urlencoded())
+app.use(cors())
 app.use(express.json())
+
+app.use('/api/todo',todoRouter)
 
 app.use(errorController.PageNotFound)
 

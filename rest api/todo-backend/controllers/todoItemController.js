f7d1905976add4e4d8880dc8d47e62b3ea0e8d89
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-18 04:02:44 +0500

    Update todoItemController.js

--- rest api/todo-backend/controllers/todoItemController.js
+++ rest api/todo-backend/controllers/todoItemController.js
@@ -1,3 +1,20 @@
+const todoItem = require("../models/todoItem");
+
 exports.postTodo = async (req,res)=>{
-    
+    console.log(req.body);
+    const {task} = req.body
+    const todos = new todoItem({task})
+    await todos.save()
+    res.status(201).json(todos)
 }
+
+exports.getTodo = async (req,res)=>{
+    const yourTodo = await todoItem.find()
+    res.json(yourTodo)
+}
+exports.deleteTodo = async (req,res)=>{
+    const {id} = req.params
+    await todoItem.findByIdAndDelete(id)
+    res.json({ message: "Deleted successfully" });
+}
+

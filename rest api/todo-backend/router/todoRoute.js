120a40e7c6e326ccd1c6d58ebac27806fb56fca9
Muhammad AYAN <m101ayan92@gmail.com>
2025-09-18 04:02:39 +0500

    Update todoRoute.js

--- rest api/todo-backend/router/todoRoute.js
+++ rest api/todo-backend/router/todoRoute.js
@@ -4,6 +4,8 @@ const todoItemController = require('../controllers/todoItemController')
 const todoRouter = express.Router()
 
 
+todoRouter.get('/',todoItemController.getTodo)
 todoRouter.post('/',todoItemController.postTodo)
+todoRouter.delete('/:id',todoItemController.deleteTodo)
 
 module.exports = todoRouter
